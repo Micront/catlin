209c00957d91db2fa935308f2ff31f14bdf03b4a
s.stepanov <micront@mail.ru>
2020-11-08 03:47:34 +0300

    version 0.2

--- app/src/main/java/com/justai/aimybox/MainActivity.kt
+++ app/src/main/java/com/justai/aimybox/MainActivity.kt
@@ -1,16 +1,17 @@
-package com.justai.aimybox.jaicf
+package com.justai.aimybox
 
+import android.content.IntentFilter
 import android.os.Bundle
-import android.view.Window
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
+import com.justai.aimybox.MyCustomSkill.Companion.ACTION_EXIT
+import com.justai.aimybox.MyCustomSkill.Companion.ACTION_UPDATE
 import com.justai.aimybox.components.AimyboxAssistantFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.layout_activity_main)
 
         window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
@@ -19,9 +20,18 @@ class MainActivity : AppCompatActivity() {
 
         supportFragmentManager.beginTransaction().apply {
             replace(R.id.assistant_container, assistantFragment)
-//            var progressBar = findViewById<ProgressBar>(R.id.progressBar)
             commit()
         }
+
+        val receiver = UpdateActivityReceiver()
+        receiver.activity = this
+        this.registerReceiver(
+            receiver, IntentFilter(ACTION_UPDATE)
+        )
+        this.registerReceiver(
+            receiver, IntentFilter(ACTION_EXIT)
+        )
+
     }
 
     override fun onBackPressed() {
@@ -30,4 +40,8 @@ class MainActivity : AppCompatActivity() {
         if (assistantFragment?.onBackPressed() != true) super.onBackPressed()
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        android.os.Process.killProcess(android.os.Process.myPid())
+    }
 }
--- app/src/main/java/com/justai/aimybox/jaicf/MainScenario.kt
+++
@@ -1,7 +0,0 @@
-package com.justai.aimybox.jaicf
-
-import com.justai.jaicf.model.scenario.Scenario
-
-object MainScenario : Scenario() {
-    //todo
-}
